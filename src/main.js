6ee7e05585ba88b042aa5705036f806f11a16a84
TitanSnow <sweeto@live.cn>
2018-06-15 23:10:19 +0800

    new bundle manner

--- src/main.js
+++ src/main.js
@@ -1,10 +1,2 @@
-import Vue from 'vue'
-import Player from './components/pupilplayer.vue'
-import { create } from 'domain';
-
-window.pupilplayer = function(el){
-  const player = Object.assign({el: el}, Player)
-  player.data = Object.assign(player.data(), {src: '', danmakus: []})
-  delete player.props
-  Object.setPrototypeOf(this, new Vue(player))
-}
+import pupilplayer from './components/pupilplayer.vue'
+window.Vue.component('pupilplayer', pupilplayer)
